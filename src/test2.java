411394b515bed4a6c8c6a2339dedf25afb0ac379
yuan1211 <j45979531@gmail.com>
2024-04-03 15:50:59 +0900

    poby

---
+++ src/test2.java
@@ -0,0 +1,8 @@
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("What is the cutest thing in the world?");
+        System.out.println("Needless to say, it's Poby");
+        System.out.println("Then everyone nodded.");
+    }
+}
